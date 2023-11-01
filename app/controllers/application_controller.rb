dbcd3af4e6a1c0d4240da960246272b9c668a09d
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2023-11-01 08:19:22 +0000

    非ログインユーザーがtopとaboutをクリックした時にリダイレクトしないように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index, :show, :edit, :update, :destroy]
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   private
 
   def after_sign_in_path_for(resource)
