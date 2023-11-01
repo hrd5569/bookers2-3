a38aaeece0b3b62676a78b9819be9520e0ad0efe
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2023-11-01 07:25:36 +0000

    bootstrap導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+# users/:idのルートパスがusers/sign_inを包括してしまわないように、devise_for :usersを先に書くことでusers/sign_in の後に　users/:idがルーティングとして動かす。
+  devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+ end
