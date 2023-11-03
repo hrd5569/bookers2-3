b64f542866479b9259179b43f04a36126570d90e
hrd5569 <146644604+hrd5569@users.noreply.github.com>
2023-11-03 15:59:52 +0000

    [Add]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ApplicationRecord
+  
   belongs_to :user
+  has_many :favorite, dependent: :destroy
+  has_one_attached :image
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  has_one_attached :profile_image
 
   has_many :books, dependent: :destroy
-  has_one_attached :profile_image
+  has_many :favorite, dependent: :destroy
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
